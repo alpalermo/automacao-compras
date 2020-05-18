3f91b5f07078e9040aaa135046c04990425db5d8
alpalermo <62974361+alpalermo@users.noreply.github.com>
2020-05-17 22:09:50 -0300

    Adicionando ExpectedConditions

--- src/stepdefinition/TestingSteps.java
+++ src/stepdefinition/TestingSteps.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -15,6 +17,7 @@ import cucumber.api.java.en.When;
 
 public class TestingSteps {
 	static WebDriver driver;
+	WebDriverWait wait = null;
 	
 	@Given("^Usuario esta na Home Page$")
 	public void user_is_on_Home_Page() {
@@ -28,60 +31,126 @@ public class TestingSteps {
 
 	@When("^Usuario busca um produto$")
 	public void usuario_busca_um_produto() throws InterruptedException {
-		WebElement buscaInput = driver.findElement(By.id("h_search-input"));
-		buscaInput.click();
-		buscaInput.clear();
-		buscaInput.sendKeys("Monitor lg ajustável");
-		WebElement buscaButton = driver.findElement(By.id("h_search-btn"));
-		buscaButton.click();		
+		try {
+			wait = new WebDriverWait(driver, 3);
+			
+			WebElement buscaInput = wait.until(
+					ExpectedConditions.elementToBeClickable(By.id("h_search-input")));
+			buscaInput.click();
+			buscaInput.clear();
+			buscaInput.sendKeys("Monitor lg ajustável");
+			WebElement buscaButton = driver.findElement(By.id("h_search-btn"));
+			buscaButton.click();
+		
+		} catch(Exception e) {
+			System.out.println("Input não encontrado!");
+		}
 	}
 	
 	@And("^Usuario escolhe um produto$")
 	public void usuario_escolhe_um_produto() throws InterruptedException {
-		WebElement produtoButton = driver.findElement(By.xpath("//a[contains(@href,'72025918')]"));
-		produtoButton.click();
+		try {
+			wait = new WebDriverWait(driver, 3);
+			
+			WebElement produtoButton = wait.until(
+					ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href,'72025918')]")));
+			produtoButton.click();			
+		} catch(Exception e) {
+			System.out.println("Botão não encontrado!");
+		}
 	}
 
 	@Then("^Produto selecionado exibido$")
 	public void produto_selecionado_exibido() {
-		WebElement produtoExibido = driver.findElement(By.xpath("//h1[@id='product-name-default' and contains(text(),'Monitor LG Ajustável')]"));
-		System.out.println("Produto exibido");
+		try {
+			wait = new WebDriverWait(driver, 3);
+			
+			WebElement produtoExibido = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h1[@id='product-name-default' and contains(text(),'Monitor LG Ajustável')]"))));
+
+			System.out.println("Produto exibido" + produtoExibido);			
+		} catch(Exception e) {
+			System.out.println("Produto não encontrado!");
+		}
+		
 	}
 
 	@When("^Usuario clica em comprar$")
 	public void usuario_clica_em_comprar() throws InterruptedException {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement comprarButton = driver.findElement(By.id("btn-buy"));
-		js.executeScript("window.scrollBy(0,100)");
-		comprarButton.click();
+		try {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			wait = new WebDriverWait(driver, 3);
+			js.executeScript("window.scrollBy(0,100)");
+			
+			WebElement comprarButton = wait.until(
+					ExpectedConditions.elementToBeClickable(By.id("btn-buy")));
+			comprarButton.click();
+		} catch(Exception e) {
+			System.out.println("Botão não encontrado!");
+		}
 	}
 	
 	@When("^Usuario clica em continuar$")
 	public void usuario_clica_em_continuar() throws InterruptedException {
-		WebElement simContinuarButton = driver.findElement(By.xpath("//a[@value='Sim, continuar']"));
-		simContinuarButton.click();
-		WebElement continuarButton = driver.findElement(By.id("btn-continue"));
-		continuarButton.click();
+		try {
+			wait = new WebDriverWait(driver, 3);
+
+			WebElement simContinuarButton = wait.until(
+					ExpectedConditions.elementToBeClickable(By.xpath("//a[@value='Sim, continuar']")));
+			simContinuarButton.click();
+
+			WebElement continuarButton = wait.until(
+					ExpectedConditions.elementToBeClickable(By.id("btn-continue")));
+			continuarButton.click();
+		} catch(Exception e) {
+			System.out.println("Botão não encontrado!");
+		}
 	}
 	
 	@Then("^Resumo produto exibido$")
 	public void resumo_produto_exibido() {
-		WebElement produtoExibido = driver.findElement(By.xpath("//a[@title='Monitor LG Ajustável 23,8” Full HD IPS LED 1920x1080 VGA HDMI DisplayPort 24BL550J']"));
-		System.out.println("Produto exibido");
+		try {
+			wait = new WebDriverWait(driver, 3);
+		
+			WebElement resumoProdutoExibido = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[@title='Monitor LG Ajustável 23,8” Full HD IPS LED 1920x1080 VGA HDMI DisplayPort 24BL550J']"))));
+
+			System.out.println("Produto exibido" + resumoProdutoExibido);
+		} catch(Exception e) {
+			System.out.println("Produto não encontrado!");
+		}
 	}
 	
 	@When("^Usuario clica em continuar apos resumo$")
 	public void usuario_clica_em_continuar_apos_resumo() throws InterruptedException {
-		WebElement continuarButton = driver.findElement(By.id("buy-button"));
-		continuarButton.click();
+		
+		try {
+			wait = new WebDriverWait(driver, 3);
+			WebElement continuarButton = wait.until(
+					ExpectedConditions.elementToBeClickable(By.id("buy-button")));
+			continuarButton.click();
+		} catch(Exception e) {
+			System.out.println("Botão não encontrado!");
+		}
 	}	
 	
 	@Then("^Tela de login do cliente exibida$")
 	public void tela_de_login_do_cliente_exibida() {
-		WebElement inputEmail = driver.findElement(By.id("email-input"));
-		WebElement inputPassword = driver.findElement(By.id("password-input"));
-		WebElement loginButton = driver.findElement(By.id("email-input"));
-		System.out.println("Tela de login exibida");
+		try {
+			wait = new WebDriverWait(driver, 3);
+		
+			WebElement inputEmail = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.id("email-input"))));
+			WebElement inputPassword = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.id("password-input"))));
+			WebElement loginButton = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.id("loginButton"))));
+
+			System.out.println("Tela de login exibida" + inputEmail + "" + inputPassword + "" + loginButton);
+		} catch(Exception e) {
+			System.out.println("Elementos não encontrados!");
+		}
+			
 	}
 	
 }
